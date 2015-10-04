ccafb27bb3f9577cff1d36b4e5e7c277e46fb2d5
cocodrips <cocodrips@gmail.com>
2015-10-04 11:47:50 +0900

    Success to apply greedy algorithm.

--- back_to_the_code/back_to_the_code/main.cpp
+++ back_to_the_code/back_to_the_code/main.cpp
@@ -28,7 +28,9 @@ const char ME = '0';
 struct Pos {
     int x = 0;
     int y = 0;
-    Pos () {}
+    Pos () {
+        Pos(-1, -1);
+    }
     Pos (int _x, int _y) {
         x = _x; y = _y;
     }
@@ -42,7 +44,7 @@ struct Pos {
     }
     
     bool operator<(const Pos &other) const {
-        return (x + y) < (other.x + other.y);
+        return (x * game::Y + y) < (other.x * game::Y + other.y);
     }
     
 public:
@@ -53,7 +55,10 @@ public:
     bool isValid() {
         return 0 <= x && x < game::X && 0 <= y && y < game::Y;
     }
-
+    
+    void dump() {
+        cerr << "Pos: (" << x << ", " << y << ")";
+    }
 };
 
 namespace std
@@ -78,7 +83,7 @@ namespace controller {
 
 
 namespace util {
-const Pos roundVectors[9] = {
+const Pos roundVectors[] = {
     Pos(1, 1),
     Pos(1, 0),
     Pos(1, -1),
@@ -88,7 +93,7 @@ const Pos roundVectors[9] = {
     Pos(0, 1),
     Pos(0, -1),
 };
-const Pos unitVectors[4] = {
+const Pos unitVectors[] = {
     Pos(1, 0),
     Pos(-1, 0),
     Pos(0, 1),
@@ -127,24 +132,35 @@ public:
     void dump() {
         REP(y, game::Y) {
             REP(x, game::X) {
-                cerr << _board[y * game::X + x];
-            } cerr << endl;
+                cerr << _board[y * game::X + x];  // cerr << (*this)[Pos(x, y)];
+            }
+            cerr << endl;
+        }
+    }
+    
+    void init(T n) {
+        REP(y, game::Y) {
+            REP(x, game::X) {
+                _board[y * game::X + x] = n;
+            }
+            cerr << endl;
         }
     }
     
     int isPainted(const Pos &startPos) {
         unordered_set<Pos> visited;
         stack<Pos> cellStack;
-        int colored = 0;
+        int colored = 1;
         visited.insert(startPos);
         cellStack.push(startPos);
-        return 0;
+        
         while (!cellStack.empty()) {
             Pos p = cellStack.top();
+            cellStack.pop();
             AREP(i, util::roundVectors) {
                 Pos next = p + util::roundVectors[i];
-                if (visited.find(next) != visited.end()) continue;
                 if (!next.isValid()) return 0;
+                if (visited.find(next) != visited.end()) continue;
                 
                 switch ((*this)[next]) {
                 case Cell::NEURAL:
@@ -152,10 +168,9 @@ public:
                     cellStack.push(next);
                     visited.insert(next);
                 case Cell::ME:
-                    break;
+                    continue;
                 default:
                     return 0;
-                    break;
                 }
             }
         }
@@ -197,17 +212,19 @@ enum Strategy {
 };
 
 struct PaintCandidate {
-    int depth;
-    int paintedCell;
+    int depth = 0;
+    int paintedCell = 0;
+    vector<Pos> path;
     bool operator<(const PaintCandidate &other) const {
-        if (paintedCell == other.paintedCell) {
-            return depth < other.depth;
-        }
-        return paintedCell < other.paintedCell;
+        return ((float)(paintedCell + depth) / depth) < ((float)(other.paintedCell + other.depth) / other.depth);
+//        if (paintedCell == other.paintedCell) {
+//            return depth < other.depth;
+//        }
+//        return paintedCell < other.paintedCell;
     }
     
     bool operator==(const PaintCandidate &other) const {
-        return  paintedCell == other.paintedCell && depth == other.depth;
+        return paintedCell == other.paintedCell && depth == other.depth;
     }
     
 };
@@ -219,7 +236,7 @@ public:
     Board<char> board;
     Board<bool> visited;
     Squid squids[game::SQUID_NUM_MAX];
-    Strategy strategy = CORNER;
+    Strategy strategy = MAX_RECT;
     Pos *nextDest = nullptr;
 
     
@@ -247,25 +264,36 @@ public:
     }
 
     
-    void largestSpace(Pos prev, Pos pos, Board<char> *board, int depth, int maxDepth, vector<PaintCandidate> *candidates) {
+    void largestSpace(Pos pos, Board<char> *board, int depth, int maxDepth, vector<PaintCandidate> *candidates) {
         if (depth == maxDepth) return;
         AREP(i, util::unitVectors) {
             Pos next = pos + util::unitVectors[i];
-            if (next == prev) continue;
             if ((*board)[next] == Cell::NEURAL) {
                 (*board)[next] = Cell::ME;
-                int result = (*board).isPainted(pos);
-                if (result == 0) {
-                    largestSpace(pos, next, board, depth + 1, maxDepth, candidates);
+                
+                int paintedSum = 0;
+                AREP(i, util::unitVectors) {
+                    Pos tmp = next + util::unitVectors[i];
+                    if (visited[tmp]) continue;
+                    int result = (*board).isPainted(tmp);
+                    paintedSum += result;
+                }
+                if (paintedSum == 0) {
+                    largestSpace(next, board, depth + 1, maxDepth, candidates);
                 } else {
                     PaintCandidate candidate;
                     candidate.depth = depth + 1;
-                    candidate.paintedCell = result;
+                    candidate.paintedCell = paintedSum;
+                    candidates -> push_back(candidate);
+                    cerr << endl;
+                    cerr << (float)(paintedSum + depth + 1) / (depth + 1)   << endl;
+//                    board->dump();
                 }
+
                 (*board)[next] = Cell::NEURAL;
-                return;
             }
         }
+        return;
     }
     
     Pos corner(Pos myPos) {
@@ -279,20 +307,62 @@ public:
        return *nextDest;
     }
     
+//    Pos nearestUnknownCell(const Pos pos) {
+//        
+//    }
+    
+    Pos unknownCell(const Pos myPos) {
+        Pos p;
+        AREP(i, util::unitVectors) {
+            Pos tmp = myPos + util::unitVectors[i];
+            if (!tmp.isValid()) continue;
+            if (p.isValid()) {
+                p = tmp;
+            }
+            
+            if (board[tmp] == Cell::NEURAL) {
+                return tmp;
+            }
+            
+            if (!visited[tmp]) {
+                return tmp;
+            }
+        }
+        return p;
+    }
+    
     Pos minRect(const Pos myPos) {
         Pos closest = minDistToEnemies(myPos);
-        int dist = myPos.dist(closest);
-        Pos maxPos = util::unitVectors[0];
+        int dist = myPos.dist(closest) / 2;
+        Pos maxPos = unknownCell(myPos);
+        cerr << "Default";
+        maxPos.dump();
         PaintCandidate maxCandidate;
         maxCandidate.paintedCell = 0; maxCandidate.depth = 20;
         AREP(i, util::unitVectors) {
+            Pos next = myPos + util::unitVectors[i];
+            if (visited[next]) continue;
             vector<PaintCandidate> candidates;
-            largestSpace(myPos, myPos + util::unitVectors[i], &board, 0, max(dist, 6), &candidates);
-            sort(candidates.begin(), candidates.end());
-            if (maxCandidate < candidates[candidates.size() - 1]) {
-                maxCandidate = candidates[candidates.size() - 1];
-                maxPos = util::unitVectors[i];
+            char tmp = board[next];
+            if (tmp == Cell::NEURAL) {
+                board[next] = Cell::ME;
             }
+            vector<Pos> path;
+            largestSpace(myPos + util::unitVectors[i], &board, 0, min(dist, 6), &candidates);
+            cerr << "candidates: " << candidates.size() << endl;
+            if (candidates.size() > 0) {
+                sort(candidates.begin(), candidates.end());
+                (myPos + util::unitVectors[i]).dump();
+                cerr << "cell:" << candidates[candidates.size() - 1].paintedCell;
+                cerr << ", depth:" << candidates[candidates.size() - 1].depth << endl;
+                if (maxCandidate < candidates[candidates.size() - 1]) {
+                    maxCandidate = candidates[candidates.size() - 1];
+                    maxPos = myPos + util::unitVectors[i];
+                }
+            }
+            cerr << "--------------------------------------" << endl;
+            board[next] = tmp;
+
         }
         return maxPos;
     }
@@ -314,17 +384,122 @@ public:
         }
         
         if (strategy == MAX_RECT) {
-            next = Pos(0, 0);//minRect(myPos);
+            next = minRect(myPos);
         }
-        cerr << strategy << endl;
+        cerr << "Strategy:" << strategy << endl;
     
         controller::printNext(next);
     }
 };
 
+//////////////////////  test  /////////////////////////////
+namespace test {
+void isPainted() {
+    Board<char> board;
+    board.init(Cell::NEURAL);
+    Pos nineBlock[] = {
+        Pos(0, 0),
+        Pos(0, 1),
+        Pos(0, 2),
+        Pos(0, 3),
+        Pos(1, 3),
+        Pos(2, 3),
+        Pos(3, 3),
+        Pos(3, 2),
+        Pos(3, 1),
+        Pos(3, 0),
+        Pos(2, 0),
+        Pos(1, 0),
+    };
+    AREP(i, nineBlock) {
+        board[nineBlock[i]] = Cell::ME;
+    }
+    cout << "isPainted - 4: " << board.isPainted(Pos(1, 1)) << endl;
+    board[Pos(3,2)] = Cell::NEURAL;
+    cout << "isPainted - 0: " << board.isPainted(Pos(1, 1)) << endl;
+
+
+}
+    
+void largestSpace() {
+    Board<char> board;
+    board.init(Cell::NEURAL);
+    Pos nineBlock[] = {
+        Pos(0, 0),
+        Pos(0, 1),
+        Pos(0, 2),
+        Pos(0, 3),
+        Pos(1, 3),
+        Pos(3, 3),
+        Pos(3, 2),
+        Pos(3, 1),
+        Pos(3, 0),
+        Pos(2, 0),
+        Pos(1, 0),
+    };
+    AREP(i, nineBlock) {
+        board[nineBlock[i]] = Cell::ME;
+    }
+    
+    Battle *battle = new Battle();
+    vector<PaintCandidate> candidates;
+    battle -> largestSpace(Pos(1, 3), &board, 0, 7, &candidates);
+    cout << "largestSpace - 0: " << candidates.size() << endl;
+    
+    board[Pos(2, 3)] = Cell::ME;
+    vector<PaintCandidate> candidates2;
+    battle -> largestSpace(Pos(3, 3), &board, 0, 6, &candidates2);
+    cout << "largestSpace - 2: " << candidates.size() << endl;
+
+}
+    
+void largestSpace2() {
+    Board<char> board;
+    board.init(Cell::NEURAL);
+    Pos nineBlock[] = {
+        Pos(10, 1),
+        Pos(11, 1),
+        Pos(12, 1),
+        Pos(12, 2),
+        Pos(12, 3),
+        Pos(12, 4),
+        Pos(11, 4),
+        Pos(10, 4),
+        Pos(9, 4),
+        Pos(8, 4),
+        Pos(7, 4),
+        Pos(7, 3),
+        Pos(7, 2),
+        Pos(7, 1),
+    };
+    AREP(i, nineBlock) {
+        board[nineBlock[i]] = Cell::ME;
+    }
+    
+    Battle *battle = new Battle();
+    vector<PaintCandidate> candidates;
+    battle -> largestSpace(Pos(7, 1), &board, 0, 7, &candidates);
+    cout << "largestSpace2 - 0: " << candidates.size() << endl;
+//    sort(candidates.begin(), candidates.end());
+    REP(i, candidates.size()) {
+        cerr << candidates[i].paintedCell << "," << candidates[i].depth << endl;
+    }
+    
+    
+}
+    
+void runner() {
+    test::isPainted();
+    test::largestSpace();
+    test::largestSpace2();
+}
+}
+
 ////////////////////// mainloop ///////////////////////////
 
 int main(int argc, const char * argv[]) {
+//    test::runner(); return 0;
+    
     int enemyNum = 0;
     cin >> enemyNum; cin.ignore();
     Battle battle = Battle();
